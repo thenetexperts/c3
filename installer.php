be391c0f6026a9939596b3ae12e865dbcc77f477
thenetexperts <ahmann@thenetexperts.info>
2015-02-23 16:40:43 +0100

    Create installer.php

---
+++ installer.php
@@ -0,0 +1,12 @@
+<?php
+namespace Codeception\c3;
+use \Composer\Script\Event;
+class Installer
+{
+    public static function copyC3ToRoot(Event $event) {
+        $io = $event->getIO();
+        $io->write("<info>[c3]</info> Copying c3.php to the root of your project...");
+        copy(__DIR__.DIRECTORY_SEPARATOR.'c3.php', getcwd().DIRECTORY_SEPARATOR.'c3.php');
+        $io->write("<info>[c3]</info> Include c3.php into index.php in order to collect codecoverage from server scripts");
+    }
+}
